17619e0183dcb2dc7467e4718aa71e8606a8d7f7
prsshahrivar <prsshahrivar@gmail.com>
2023-07-29 18:10:11 +0330

    New Design for PageNotFound

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,17 @@
-import PageNav from "../components/PageNav";
+import {useNavigate} from "react-router-dom";
+import styles from "./PageNotFound.module.css"
+import Button from "../components/Button";
 
 export default function PageNotFound() {
+
+    const navigator = useNavigate()
+
     return (
-        <div>
-            <PageNav/>
-            <h1>Page not found 😢</h1>
+        <div className={styles.container}>
+
+            <h1 className={styles.errorText}>404 : Page not found </h1>
+            <Button type='primary' onClick={() => navigator('/')}> Home </Button>
+
         </div>
     );
 }
